0b3437819a69acdab1c6fc3bdade4474ba442282
mfhenao <mf.henao10@ciaf.edu.co>
2024-04-17 19:54:15 -0500

    ejercicio2 OK

---
+++ ejercicio2.js
@@ -0,0 +1,12 @@
+let numero1= prompt("dame el primer numero: ");
+let numero2= prompt("dame el segundo numero: ");
+
+if (numero1 > numero2) {
+    alert("el primer numero es mayor que el segundo");
+    } else if (numero2 > numero1) {
+    alert("el segundo numero es mayor que el primero");
+    } else {
+    alert("los numeros son iguales");
+    }
+
+
